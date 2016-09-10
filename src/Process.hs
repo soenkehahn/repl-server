0375ec8a453a3f4e2d739a68cc38e3f132f4af76
Sönke Hahn <soenkehahn@gmail.com>
2016-09-10 13:44:08 -0400

    add module Process

---
+++ src/Process.hs
@@ -0,0 +1,24 @@
+
+module Process where
+
+import           Control.Exception
+import           System.IO
+import           System.Process
+
+-- | Runs an external process while the given action is being executed.
+withProcess :: String -> ((Handle, Handle, ProcessHandle) -> IO a) -> IO a
+withProcess command action = do
+  -- installHandler sigINT (CatchOnce (terminateProcess processHandle)) Nothing
+  let process = (shell command){
+        std_in = CreatePipe,
+        std_out = CreatePipe
+      }
+  bracket (createProcess process) killProcess $
+    \ (Just pStdIn, Just pStdOut, Nothing, processHandle) -> do
+      hSetBuffering pStdIn NoBuffering
+      hSetBuffering pStdOut NoBuffering
+      action (pStdIn, pStdOut, processHandle)
+  where
+    killProcess (_, _, _, process) = do
+      terminateProcess process
+      waitForProcess process
---
+++ test/ProcessSpec.hs
@@ -0,0 +1,40 @@
+
+module ProcessSpec where
+
+import           Control.Concurrent
+import           Control.Monad
+import           Data.List
+import           System.Directory
+import           System.Exit
+import           System.IO
+import           System.Process
+import           Test.Hspec
+import           Test.Mockery.Directory
+
+import           Process
+
+spec = around_ inTempDirectory $ do
+  describe "withProcess" $ do
+    it "runs the given process concurrently" $ do
+      withProcess "touch file" $ \ (to, from, _) -> do
+        let waitForFile = do
+              files <- getDirectoryContents "."
+              when (not ("file" `elem` files)) $
+                waitForFile
+        waitForFile
+      readFile "file" `shouldReturn` ""
+
+    it "kills the process when the invoking thread is killed" $ do
+      mvar <- newEmptyMVar
+      withProcess "ghci" $ \ (_, _, process) -> do
+        putMVar mvar process
+      process <- takeMVar mvar
+      getProcessExitCode process `shouldReturn` Just (ExitFailure (- 15))
+
+    it "allows to interact with stdin and stdout of the process" $ do
+      withProcess "ghci" $ \ (to, from, _) -> do
+        hPutStrLn to "map succ \"aaa\""
+        hGetLine from -- skipping one line of ghci info
+        result <- hGetLine from
+        result `shouldSatisfy` ("bbb" `isInfixOf`)
+        hPutStrLn to ":quit"
