ed9c4a4f85629fc1601e762c8ac4123957cd2718
Sönke Hahn <soenkehahn@gmail.com>
2016-10-28 12:53:48 -0400

    refactor passing of process handles

--- src/ReplServer.hs
+++ src/ReplServer.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module ReplServer where
@@ -38,33 +39,41 @@ instance HasArguments Config
 
 replServer :: Config -> IO ()
 replServer config = do
-  withProcess (replCommand config) $ \ (to, from, fromStdErr, _) -> do
+  withProcess (replCommand config) $ \ (to, from, errFrom, _) -> do
+    let handles = ProcessHandles{to, from, errFrom}
     _ <- toNextPrompt config from
-    _ <- executeReplAction config to from fromStdErr Nothing
-    withApplication (app config to from fromStdErr) $ do
+    _ <- executeReplAction config handles Nothing
+    withApplication (app config handles) $ do
       forever $ threadDelay 1000000
 
 toNextPrompt :: Config -> Handle -> IO String
 toNextPrompt config from = do
   readUntil (replPrompt config) from
 
-executeReplAction :: Config -> Handle -> Handle -> Handle -> Maybe Text -> IO String
-executeReplAction config to from fromStdErr mAction = do
-  (errOutput, stdOutput) <- captureWhile fromStdErr $ do
+data ProcessHandles
+  = ProcessHandles {
+    to :: Handle,
+    from :: Handle,
+    errFrom :: Handle
+  }
+
+executeReplAction :: Config -> ProcessHandles -> Maybe Text -> IO String
+executeReplAction config ProcessHandles{to, from, errFrom} mAction = do
+  (errOutput, stdOutput) <- captureWhile errFrom $ do
     hPutStrLn to (fromMaybe (replAction config) (fmap cs mAction)) >> hFlush to
     toNextPrompt config from
   hPutStrLn stderr errOutput >> hFlush stderr
   return (errOutput ++ "\n===\n" ++ stdOutput)
 
-app :: Config -> Handle -> Handle -> Handle -> Application
-app config to from fromStdErr = serve api $ server config to from fromStdErr
+app :: Config -> ProcessHandles -> Application
+app config handles = serve api $ server config handles
 
-server :: Config -> Handle -> Handle -> Handle -> Server Api
-server config to from fromStdErr = postAction config to from fromStdErr
+server :: Config -> ProcessHandles -> Server Api
+server = postAction
 
-postAction :: Config -> Handle -> Handle -> Handle -> Maybe Text -> Handler Text
-postAction config to from fromStdErr mAction = liftIO $ do
-  output <- executeReplAction config to from fromStdErr mAction
+postAction :: Config -> ProcessHandles -> Maybe Text -> Handler Text
+postAction config handles mAction = liftIO $ do
+  output <- executeReplAction config handles mAction
   return $ cs output
 
 withApplication :: Application -> IO a -> IO a
